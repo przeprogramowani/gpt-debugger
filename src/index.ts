6cb88cd8c8959840995f4ab77c38b04e3e45c8bc
psmyrdek <przemek.smyrdek@gmail.com>
2024-03-28 09:04:55 +0100

    feat: update

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,7 @@ async function run(): Promise<void> {
     - Do not include any sensitive information.
     - Do not repeat the same information.
     - List files that are relevant to the error.
+    - If the log is empty, do not provide a solution. Terminate the conversation.
     - Instead of Markdown, use format readable in bash terminal.
 
     The error log is wrapped with the tag ERROR_LOG.
